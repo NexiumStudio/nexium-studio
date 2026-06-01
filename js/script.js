d6a61cfafcc6e9304c9f86935d48b02ff4f495df
NexiumStudio <nexium.studio1@gmail.com>
2026-06-01 14:41:35 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
 // Script da nexium Studio
+const elementos = document.querySelectorAll("header, section, footer");
+
+elementos.forEach((elemento) => {
+    elemento.classList.add("hidden");
+});
+
+const observador = new IntersectionObserver((entradas) => {
+    entradas.forEach((entrada) => {
+        if (entrada.isIntersecting) {
+            entrada.target.classList.add("show");
+        }
+    });
+});
+
+elementos.forEach((elemento) => {
+    observador.observe(elemento);
+});
